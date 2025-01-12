cbf9db2f7ad68c080c5135c9ae86eb8bb8a38c01
Frontast <58586034+Frontast@users.noreply.github.com>
2025-01-12 22:20:31 +0500

    Array task completed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -42,5 +42,25 @@
                 Console.WriteLine(Convert.ToString(a ^ b, 16));
                 break;
         }
+
+        //Домашнее задание по массивам
+        //Задача "А"
+
+        int[] arrFib = new int[] {0,1,1,2,3,5,8,13};
+        string[] arrMonths = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int[][] arrTwo = new int[3][] { new[] {2, 3, 4}, new[] {4, 9, 16}, new[] {8, 27, 64} };
+        double[][] arrMath = new double[3][] { new[] {1.0,2.0,3.0,4.0,5.0}, new[] {Math.E, Math.PI}, new[] {Math.Log10(1), Math.Log10(10), Math.Log10(100), Math.Log10(1000)} };
+
+        //Задача "Б"
+
+        int[] array = {1, 2, 3, 4, 5};
+        int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
+        Array.Copy(array, array2, 3);
+        for (int i = 0; i < array2.Length; i++)
+        {
+            Console.WriteLine(array2[i]);
+        }
+        Array.Resize(ref array, array.Length * 2);
+        Console.WriteLine(array.Length);
     }
 }
